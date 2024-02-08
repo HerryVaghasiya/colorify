ffcb58b2e6512453cbb7367d52299c32b13f945b
HerryVaghasiya <herry@technous.in>
2024-02-08 15:51:46 +0530

    add sum

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 export class Log{
-    static success(msg: string){
-        console.log(`%c ${msg}`,'color:green');
+    // static success(msg: string){
+    //     console.log(`%c ${msg}`,'color:green');
+    // }
+    // static danger(msg: string){
+    //     console.log(`%c ${msg}`,'color:red');
+    // }
+    // static info(msg: string){
+    //     console.log(`%c ${msg}`,'color:black; background:yellow');
+    // }
+    static sum(num:number,num1:number){
+        var sum=num + num1;
+    console.log(`number${num} + number ${num1} = ${sum}`)
     }
-    static danger(msg: string){
-        console.log(`%c ${msg}`,'color:red');
-    }
-    static info(msg: string){
-        console.log(`%c ${msg}`,'color:black; background:yellow');
-    }
+    
+   
 }
